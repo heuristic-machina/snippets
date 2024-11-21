8c233888332b94ca25e45cc657c58c7a20b709f8
heuristic_machina <machine_learner@yahoo.com>
2024-11-20 18:15:43 -0700

    fetching snippet loading feature added

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -8,9 +8,13 @@ interface SnippetShowPageProps {
   }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
-
+    //create pause for loading.tsx feature
+    //delay 2 seconds
+    await new Promise((r) => setTimeout(r, 2000));
     const { id } = await props.params;
  
+    //url recognizes the query as a string but the idea is type int
+    //parseInt used to remove error
     const snippet = await db.snippet.findFirst({
       where: { id: parseInt(id) },
     });
