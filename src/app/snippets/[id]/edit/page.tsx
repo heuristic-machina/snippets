c2e18d4860c3a5f3de96ee7e8d3a4e76cdf8f543
heuristic_machina <machine_learner@yahoo.com>
2024-11-21 03:37:48 -0700

    notFound next/navigation implemented in [id] edit

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,10 +1,24 @@
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
 interface SnippetEditPageProps {
-    params: {
-        id: string;
-    }
+    params: Promise<{
+      id: string;
+    }>;
   }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
-    const id = parseInt(props.params.id);
-    return <div>Editing snippet with id {id}</div>
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+    const { id } = await props.params;
+ 
+    const snippetId = parseInt(id);
+    const snippet = await db.snippet.findFirst({
+      where: { id: snippetId },
+    });
+
+    if (!snippet) {
+        return notFound();
+    }
+  
+    // return <div>Editing snippet with id {id}</div>
+    return <div>Editing snippet with title {snippet.title}</div>
     };
