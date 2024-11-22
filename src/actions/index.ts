03819f378b22c0ed93b4d6e700cc19e3f058e7e2
heuristic_machina <machine_learner@yahoo.com>
2024-11-21 17:30:41 -0700

    update snippet server action and redirect added

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,16 @@
 'use server'
 import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
 export async function editSnippet(id: number, code: string) {
     //wire up to client
-    console.log(id, code);
+    // console.log(id, code);
 
-    //code to update snippet
-
+    //code to update snippet using Prisma
+    await db.snippet.update({
+        where: { id },
+        data: { code }
+    })
     //code to redirect user
+    redirect(`/snippets/${id}`);
 }
